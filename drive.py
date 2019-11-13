cb621a807fbb7f95dc3439dfc9807db0a7e8a3aa
Greg Clarke <greg.clarke@shopify.com>
2019-11-13 18:43:15 -0500

    drive script

---
+++ drive.py
@@ -0,0 +1,51 @@
+import curses
+import time
+from adafruit_motorkit import MotorKit
+
+kit = MotorKit()
+
+screen = curses.initscr()
+
+curses.noecho()
+curses.cbreak()
+
+screen.keypad(True)
+
+def go():
+  kit.motor1.throttle = 0.7
+  time.sleep(0.3)
+  kit.motor1.throttle = 0.8
+  time.sleep(0.3)
+  kit.motor1.throttle = 0.9
+  time.sleep(0.3)
+  kit.motor1.throttle = 1.0
+
+def stop():
+  kit.motor1.throttle = 0.9
+  time.sleep(0.8)
+  kit.motor1.throttle = 0.8
+  time.sleep(0.6)
+  kit.motor1.throttle = 0.7
+  time.sleep(0.4)
+  kit.motor1.throttle = 0.6
+  time.sleep(0.2)
+  kit.motor1.throttle = 0
+
+try:
+  while True:
+    char = screen.getch()
+    if char == ord('q'):
+      break
+    elif char == curses.KEY_UP:
+      screen.addstr(0, 0, 'go  ')
+      go()
+    elif char == curses.KEY_DOWN:
+      screen.addstr(0, 0, 'stop ')
+      stop()
+finally:
+  curses.nocbreak()
+  screen.keypad(0)
+  curses.echo()
+  curses.endwin()
+  GPIO.cleanup()
+  p.stop()
