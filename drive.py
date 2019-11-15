3789361ebf4603985480ccd9d050fd2a7bd64fe7
Greg Clarke <greg.clarke@shopify.com>
2019-11-14 23:06:59 -0500

    yolo merge

--- drive.py
+++ drive.py
@@ -2,7 +2,131 @@ import curses
 import time
 from adafruit_motorkit import MotorKit
 
-kit = MotorKit()
+class Conductor:
+
+  def __init__(self): 
+    self.kit = MotorKit()
+    self.speed = 0
+
+  def increase_speed():
+    if -0.5 < self.speed < 0.5:
+      self.speed = 0.5
+    elif self.speed < 1:
+      self.speed += 0.1
+    self.kit.motor1.throttle = self.speed
+
+  def decrease_speed():
+    if -0.5 < self.speed < 0.5:
+      self.speed = -0.5
+    if self.speed > -1:
+      self.speed -= 0.1
+    self.kit.motor1.throttle = self.speed
+
+  def stop():
+    self.speed = 0
+    self.kit.motor1.throttle = self.speed
+
+  def draw():
+    train = """
+                [~]
+              | | (~)  (~)  (~)    /~~~~~~~~~~~~
+          /~~~~~~~~~~~~~~~~~~~~~~~  [~_~_] |    * * * /~~~~~~~~~~~|
+        [|  %___________________           | |~~~~~~~~            |
+          \[___] ___   ___   ___\  No. 4   | |   Hercie Express   |
+        /// [___+/-+-\-/-+-\-/-+ \\_________|=|____________________|=
+      //// @-=-@ \___/ \___/ \___/  @-==-@      @-==-@      @-==-@
+  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"""
+  if self.speed = 1:
+    smoke = """
+                        (+++++++++++)
+                    (++++)
+                (+++)
+              (+++)
+              (++)
+  """
+  elif self.speed > 0.9:
+      smoke = """
+                    (+++++++++++)
+                  (++++)
+                (+++)
+              (+++)
+              (++)
+  """
+  elif self.speed > 0.8:
+      smoke = """
+                (+++++++++++)
+                (++++)
+              (+++)
+              (+++)
+              (++)
+  """
+  elif self.speed > 0.7:
+      smoke = """
+                (++++++)
+                (+++)
+              (++)
+              (++)
+              (++)
+  """
+  elif self.speed > 0.6:
+      smoke = """
+              (++++)
+              (+++)
+              (++)
+              (++)
+              (++)
+  """
+  elif self.speed > 0.5:
+      smoke = """
+            (++++)
+              (++)
+              (++)
+              (++)
+              (++)
+  """
+  elif self.speed > 0.4:
+      smoke = """
+
+
+
+
+
+  """
+  elif self.speed > -0.5:
+      smoke = """
+            (++++)
+              (++)
+              (++)
+              (++)
+              (++)
+  """
+  elif self.speed > -0.6:
+      smoke = """
+            (++++)
+            (+++)
+              (++)
+              (++)
+              (++)
+  """
+  elif self.speed > -0.7:
+      smoke = """
+      (++++++)
+          (+++)
+            (++)
+              (++)
+              (++)
+  """
+  else:
+      smoke = """
+  (++++++++++)
+        (++++)
+          (+++)
+            (+++)
+              (++)
+  """
+  return smoke + train
+
+
 
 screen = curses.initscr()
 
@@ -11,84 +135,9 @@ curses.cbreak()
 
 screen.keypad(True)
 
-train_stopped = """
-                                            
-                          
-                           
-                        
-                  
-            [~]
-            | | (~)  (~)  (~)    /~~~~~~~~~~~~
-         /~~~~~~~~~~~~~~~~~~~~~~~  [~_~_] |    * * * /~~~~~~~~~~~|
-       [|  %___________________           | |~~~~~~~~            |
-         \[___] ___   ___   ___\  No. 4   | |   Hercie Express   |
-      /// [___+/-+-\-/-+-\-/-+ \\_________|=|____________________|=
-    //// @-=-@ \___/ \___/ \___/  @-==-@      @-==-@      @-==-@
- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-"""
+train = Conductor()
 
-train_go = """
-                      (+++++++++++)
-                  (++++)
-               (+++)
-             (+++)
-            (++)
-            [~]
-            | | (~)  (~)  (~)    /~~~~~~~~~~~~
-         /~~~~~~~~~~~~~~~~~~~~~~~  [~_~_] |    * * * /~~~~~~~~~~~|
-       [|  %___________________           | |~~~~~~~~            |
-         \[___] ___   ___   ___\  No. 4   | |   Hercie Express   |
-      /// [___+/-+-\-/-+-\-/-+ \\_________|=|____________________|=
-    //// @-=-@ \___/ \___/ \___/  @-==-@      @-==-@      @-==-@
- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-"""
-
-train_reverse = """
-(++++++++)
-     (++++)
-       (+++)
-         (+++)
-           (++)
-            [~]
-            | | (~)  (~)  (~)    /~~~~~~~~~~~~
-         /~~~~~~~~~~~~~~~~~~~~~~~  [~_~_] |    * * * /~~~~~~~~~~~|
-       [|  %___________________           | |~~~~~~~~            |
-         \[___] ___   ___   ___\  No. 4   | |   Hercie Express   |
-      /// [___+/-+-\-/-+-\-/-+ \\_________|=|____________________|=
-    //// @-=-@ \___/ \___/ \___/  @-==-@      @-==-@      @-==-@
- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-"""
-
-screen.addstr(0, 0, train_stopped)
-
-def go():
-  kit.motor1.throttle = 0.7
-  time.sleep(0.3)
-  kit.motor1.throttle = 0.8
-  time.sleep(0.3)
-  kit.motor1.throttle = 0.9
-  time.sleep(0.3)
-  kit.motor1.throttle = 1.0
-
-def stop():
-  kit.motor1.throttle = 0.9
-  time.sleep(0.8)
-  kit.motor1.throttle = 0.8
-  time.sleep(0.8)
-  kit.motor1.throttle = 0.7
-  time.sleep(0.8)
-  kit.motor1.throttle = 0.6
-  time.sleep(0.6)
-  kit.motor1.throttle = 0.55
-  time.sleep(0.4)
-  kit.motor1.throttle = 0
-
-def reverse():
-  kit.motor1.throttle = 0
-  time.sleep(0.2)
-  kit.motor1.throttle = -0.7
-  time.sleep(0.3)
-  kit.motor1.throttle = -0.8
+screen.addstr(0, 0, train.draw())
 
 try:
   while True:
@@ -96,50 +145,17 @@ try:
     if char == ord('q'):
       break
     elif char == curses.KEY_UP:
-      screen.addstr(0, 0, train_go)
-      go()
+      train.increase_speed()
+      screen.addstr(0, 0, train.draw())
     elif char == curses.KEY_DOWN:
-      screen.addstr(0, 0, train_stopped)
-      stop()
-    elif char == ord('r'):
-      screen.addstr(0, 0, train_reverse)
-      reverse()
-    elif char == ord('`'):
-      screen.addstr(0, 0, train_stopped)
-      kit.motor1.throttle = 0
-    elif char == ord('1'):
-      screen.addstr(0, 0, train_go)
-      kit.motor1.throttle = 0.55
-    elif char == ord('2'):
-      screen.addstr(0, 0, train_go)
-      kit.motor1.throttle = 0.6
-    elif char == ord('3'):
-      screen.addstr(0, 0, train_go)
-      kit.motor1.throttle = 0.65
-    elif char == ord('4'):
-      screen.addstr(0, 0, train_go)
-      kit.motor1.throttle = 0.7
-    elif char == ord('5'):
-      screen.addstr(0, 0, train_go)
-      kit.motor1.throttle = 0.75
-    elif char == ord('6'):
-      screen.addstr(0, 0, train_go)
-      kit.motor1.throttle = 0.8
-    elif char == ord('7'):
-      screen.addstr(0, 0, train_go)
-      kit.motor1.throttle = 0.85
-    elif char == ord('8'):
-      screen.addstr(0, 0, train_go)
-      kit.motor1.throttle = 0.9
-    elif char == ord('9'):
-      screen.addstr(0, 0, train_go)
-      kit.motor1.throttle = 0.95
-    elif char == ord('0'):
-      screen.addstr(0, 0, train_go)
-      kit.motor1.throttle = 1
+      train.decrease_speed()
+      screen.addstr(0, 0, train.draw())
+    elif char == ord(' ')
+      train.stop()
+      screen.addstr(0, 0, train.draw())
 finally:
   curses.nocbreak()
   screen.keypad(0)
   curses.echo()
   curses.endwin()
-  kit.motor1.throttle = 0
+  train.stop()
